e1ac5a03eb8230a2225fd5cca809db813e271f0a
erwin132 <erwin@val.id>
2024-06-06 10:02:27 +0000

    fix convert2openvino load model from env

--- convert2openvino.py
+++ convert2openvino.py
@@ -1,7 +1,11 @@
 from ultralytics import YOLO
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 # Load a YOLOv8n PyTorch model
-model = YOLO("/home/valid/Documents/Visto/gundu-ai/models/best-6-class-tuned.pt")
+model = YOLO(os.getenv('BASE_PATH') + '/models/best-6-class-tuned.pt')
 
 # Export the model
 model.export(format="openvino")  # creates 'yolov8n_openvino_model/'
